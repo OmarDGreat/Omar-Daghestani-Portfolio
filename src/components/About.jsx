7e66d87525bc41188511b96bcdf15c87df1c1c4c
OmarD <omar.daghestani@colorado.edu>
2025-01-12 17:52:54 -0700

    Initialized

---
+++ src/components/About.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import AboutMe from '../assets/icon.jpg'
+
+function About() {
+  return (
+    <div className='bg-black text-white py-20' id='about'>
+      <div className='container mx-auto px-8 md:px-16 lg:px-24'>
+        <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
+        
+        <div className='flex flex-col md:flex-row items-center md:space-x-12'>
+          <img src={AboutMe} alt="" 
+          className='w-80 h-90 rounded object-cover mb-8 md:mb-0'/>
+
+          <div className="flex-1">
+            <p className='text-lg mb-8'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam cupiditate ipsa minima
+              dicta tenetur similique, quibusdam reprehenderit possimus dolores at, provident molestiae
+              neque aut pariatur laudantium odit fugiat? Quidem, dicta!
+            </p>
+
+            <div className='space-y-4'>
+              <div className='flex items-center'>
+                <label htmlFor="htmlandcss" className='w-2/12'>HTML and CSS</label>
+                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                  <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 
+                  rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12'>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='space-y-4'>
+              <div className='flex items-center'>
+                <label htmlFor="htmlandcss" className='w-2/12'>React</label>
+                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                  <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 
+                  rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12'>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='space-y-4'>
+              <div className='flex items-center'>
+                <label htmlFor="htmlandcss" className='w-2/12'>Node</label>
+                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                  <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 
+                  rounded-full transform transition-transform duration-300 hover:scale-105 w-8/12'>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='mt-12 flex justify-evenly text-center'>
+              <div>
+                <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>
+                  3+
+                </h3>
+                <p>Years Experience</p>
+              </div>
+
+              <div>
+                <h3 className='text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>
+                  50+
+                </h3>
+                <p>Projects</p>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
